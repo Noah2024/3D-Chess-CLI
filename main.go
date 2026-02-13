b7309bf20c78639bc00c56bd77002cc540d34007
Noah Yurasko <ndyurasko@gmail.com>
2026-02-12 23:38:18 -0500

    updated prevent moves, added offset to prevent self elimination

--- main.go
+++ main.go
@@ -32,6 +32,14 @@ func (cli *CLI) handleCommand(cmd string, game *GAME) {
 		} else {
 			fmt.Printf("move command takes exactly one argument '%s'\n", cmd)
 		}
+	case "get":
+		if len(parsedCmd) == 2 {
+			game.getSpace(parsedCmd[1])
+		} else {
+			fmt.Printf("get command takes exactly one argument '%s'\n", cmd)
+		}
+	// case: "debug":
+	// 	game.
 	default:
 		fmt.Printf("Unknown Command '%s'\n", cmd)
 	}
@@ -51,6 +59,7 @@ type GAME struct {
 	allPieces [32]PIECE
 	boardSize intvec
 	board     [8][8]rune
+	colorMap  [8][8]rune
 }
 
 type PIECE struct {
@@ -60,6 +69,13 @@ type PIECE struct {
 
 func (game *GAME) initPieces() {
 	fmt.Println("Init Pieces")
+	//Prolly a more efficent way to initalize the pieces and board at the same time
+	//But for now this should work
+	for i := 0 + 1; i < game.boardSize.x; i += 1 {
+		for j := 0 + 1; j < game.boardSize.y; j += 1 {
+			game.board[i][j] = 0
+		}
+	}
 	x, y, i := 0, 0, 0 //I is the piece index
 	for _, character := range "RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr" {
 		if character == '/' {
@@ -73,7 +89,7 @@ func (game *GAME) initPieces() {
 				name: character,
 				pos:  intvec{x, y, 0},
 			}
-			game.board[x][y] = character
+			game.board[y][x] = character
 			i++
 		}
 		x += 1
@@ -82,9 +98,26 @@ func (game *GAME) initPieces() {
 
 // VIEW BOARD MUST BE CHANGED TO MOVE TO 3D
 func (game *GAME) viewBoard() {
-	for _, rank := range game.board {
-		fmt.Println(rank)
+	fmt.Println("    A B C D E F G H   ")
+	for i, rank := range game.board {
+		fmt.Print(string('0'+i) + " | ")
+		for j, space := range rank {
+			//I don't like preforming this many checks when rendering the board, but im not sure what other alternatives I have
+			if game.colorMap[j][i] == '1' {
+				fmt.Printf("\x1b[32m%c\x1b[0m", space+'0')
+				return
+			}
+			if space == 0 {
+				fmt.Print("0") //Must hadle 0s differently cuase otherwise they print as blank spaces string(space + '0')
+			} else {
+				fmt.Print(string(space))
+			}
+			fmt.Print(" ")
+		}
+		fmt.Print("|")
+		fmt.Println()
 	}
+
 }
 
 // NOTE TO SELF< MUST CHANGE BOUNDRIES ON FUNCS BEFORE MAKING 3D
@@ -102,8 +135,8 @@ func (game *GAME) decodeGmPos(pos string) (int, int, error) {
 		return 0, 0, fmt.Errorf("invlaid row")
 	}
 
-	x := int(col - 'a')
-	y := int(row - '0')
+	x := int(row - '0')
+	y := int(col - 'a')
 	return x, y, nil
 }
 
@@ -115,18 +148,63 @@ func (game *GAME) getTeam(piece rune) bool {
 	}
 }
 
-func (game *GAME) preventMoveThrough(start intvec, end intvec) bool {
+func (game *GAME) getSpace(pos string) {
+	game.decodeGmPos(pos)
+	x, y, error1 := game.decodeGmPos(pos)
+	if error1 != nil {
+		fmt.Printf("Error in reading space %s \n", pos)
+	} else {
+		fmt.Printf("Space X:%s - Y:%s contains %c \n", x, y, game.board[x][y])
+	}
+}
+
+func (game *GAME) addHilightSpace(x int, y int) {
+	//If I want to I could later add differnet colors, but this is mostly just for debug purposes
+	// x, y, error1 := game.decodeGmPos(pos)
+	// if error1 != nil {
+	// 	fmt.Println("Could not find space to highlight")
+	// }
+	game.colorMap[x][y] = 1 //"\x1b[32m%c\x1b[0m"
+}
+
+func (game *GAME) preventMoveThrough(start intvec, end intvec, xDif int, yDif int) bool {
 	//May need to modify for the bishop, or at least do more testing later
-	for i := start.x + 1; i <= end.x; i += 1 {
-		for j := start.y + 1; j <= end.y; j += 1 {
+	fmt.Println(game.colorMap)
+	//Need to offset so it dosn't prevent moves with itself
+	//However this needs to be offset in the direction of motion, hence the need for xDif and yDif
+	var xOffSet int
+	var yOffSet int
+	if xDif == 0 {
+		xOffSet = 0
+	} else if xDif > 0 {
+		xOffSet = 1
+	} else {
+		xOffSet = -1
+	}
+	if yOffSet == 0 {
+		yOffSet = 0
+	} else if yDif > 0 {
+		yOffSet = 1
+	} else {
+		yOffSet = -1
+	}
+	fmt.Println("Offsets are: ", xOffSet, yOffSet)
+	for i := start.x + xOffSet; i <= end.x; i += 1 {
+		for j := start.y + yOffSet; j <= end.y; j += 1 {
+			game.addHilightSpace(i, j)
 			if game.board[i][j] != 0 {
+				fmt.Printf("PROBLEM AT %d, %d \n", j, i)
+				fmt.Print(string(game.board[i][j]))
 				return false
 			}
+			fmt.Printf("HILIGHT %c \n", game.colorMap[i][j])
 		}
 	}
+	game.viewBoard()
 	return true
 }
 
+// Using != as exclusive or in this function
 func (game *GAME) validateMoves(pieceToMove rune, mvFrom intvec, mvTo intvec) bool {
 	team := game.getTeam(pieceToMove)
 	xDif := mvTo.x - mvFrom.x
@@ -144,6 +222,16 @@ func (game *GAME) validateMoves(pieceToMove rune, mvFrom intvec, mvTo intvec) bo
 		fmt.Println("Moving Knigt")
 	case 'B':
 		fmt.Println("Moving Bishop")
+		if !((xDif != 0) == (yDif != 0)) {
+			fmt.Println("Invalid Bishop Move")
+			fmt.Println("The Bishop Cannont Move like that")
+			return false
+		}
+		if !game.preventMoveThrough(mvFrom, mvTo, xDif, yDif) {
+			fmt.Println("Invalid Bishop Move")
+			fmt.Println("Can not move thorugh another piece")
+			return false
+		}
 	case 'R':
 		fmt.Println("Moving Rook")
 		if !((xDif == 0) != (yDif == 0)) {
@@ -151,19 +239,11 @@ func (game *GAME) validateMoves(pieceToMove rune, mvFrom intvec, mvTo intvec) bo
 			fmt.Println("The Rook Cannont Move like that")
 			return false
 		}
-		if !game.preventMoveThrough(mvFrom, mvTo) {
+		if !game.preventMoveThrough(mvFrom, mvTo, xDif, yDif) {
 			fmt.Println("Invalid Rook Move")
 			fmt.Println("Can not move thorugh another piece")
 			return false
 		}
-
-		fmt.Println(xDif, yDif)
-		// if (mvFrom.x == mvTo.x && mvFrom.y != mvTo.y) || (mvFrom.y == mvTo.y && mvFrom.x != mvTo.x) {
-		// 	fmt.Println("Validated Rook Move")
-		// } else {
-		// 	fmt.Println("Invalid Rook Move")
-		// }
-
 	case 'Q':
 		fmt.Println("Moving Queen")
 	case 'K':
@@ -196,10 +276,11 @@ func (game *GAME) move(cmd string) {
 		fmt.Println("No piece exists")
 		return
 	}
-
 	if game.validateMoves(pieceToMove, intvec{mvFrmX, mvFrmY, 0}, intvec{mvToX, mvToY, 0}) {
 		game.board[mvToX][mvToY] = pieceToMove
 		game.board[mvFrmX][mvFrmY] = 0
+		fmt.Printf("Moved Piece %+v to X: %s, Y:%s \n", pieceToMove, mvToX, mvToY)
+
 	} else {
 		fmt.Println("Could Not Move Piece: Check above for reason")
 	}
@@ -222,8 +303,6 @@ func (game *GAME) move(cmd string) {
 	// case 'K':
 	// 	fmt.Println("Moving King")
 	// }
-
-	fmt.Printf("Moved Piece %+v to X: %s, Y:%s \n", pieceToMove, mvToX, mvToY)
 }
 
 // GAME LOGIC DONE
